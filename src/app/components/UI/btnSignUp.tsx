6885a8e6f70283c9aeeebbc1bcbee03c9b65d0ad
RIKO-FERNANDA-S <rikofsaputra000@gmail.com>
2025-10-31 23:35:03 +0700

    percobaan git push linux

--- src/app/components/UI/btnSignUp.tsx
+++ src/app/components/UI/btnSignUp.tsx
@@ -4,7 +4,7 @@ import React from 'react'
 
 function btnSignUp() {
   return (
-    <main className="flex py-2 px-5 bg-[#0065B7] justify-center items-center rounded-xl shadow-button ">
+    <main className="flex py-2 px-5 bg-[#0065B7] justify-center items-center rounded-xl hover:shadow-button ">
       <Link href="/404">
     <h1 className="mr-2 text-white ">Sign Up</h1>
       </Link>
